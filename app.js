0f35bb5d6c6be0f37b6c330f5205737709aa0fef
SimoneChiodo <simochi.005@gmail.com>
2025-01-10 16:22:18 +0100

    add public folder, middlewares

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const app = express();
 // INIT ENV
 const { APP_HOST, APP_PORT } = process.env;
 
-//REGISTERING ROUTES
+// REGISTERING MIDDLEWARES
+app.use(express.json());
+app.use(express.static("public"));
+
+// REGISTERING ROUTES
 const moviesRouter = require("./routers/moviesRouter");
 app.use(moviesRouter);
+
+// ERRORS HANDLERS
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
+app.use(errorsHandler);
+app.use(notFound);
 
 // SERVER LISTENING
 app.listen(APP_PORT, () => {
